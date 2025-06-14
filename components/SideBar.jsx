6d4b88cd33e6f0edd6b89f3ee98f273a9216dff4
Daksh129 <dakshagrawal200312@gmail.com>
2025-06-15 00:00:25 +0530

    first commit

---
+++ components/SideBar.jsx
@@ -0,0 +1,38 @@
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const SideBar = ({expand, setExpand}) => {
+  return (
+    <div className={`flex flex-col justify-between bg-[#212327] pt-7 transition-all z-50 max-md:absolute max-md:h-screen ${expand ? "p-4 w-64" : "md:w-20 w-0 max-md:overflow-hidden"}`}>
+        <div>
+            <div className={`flex ${expand ? "flex-row gap-10" : "flex-col items-center gap-8"}`}>
+
+                <Image className={expand ? "w-36" : "w-10"} 
+                src={expand ? assets.logo_text : assets.logo_icon} alt=''/>
+                
+                <div onClick={() => expand ? setExpand(false) : setExpand(true)}>
+                    <Image src={assets.menu_icon} alt='' className='md:hidden' />
+                    <Image src={expand ? assets.sidebar_close_icon : assets.sidebar_icon} alt='' className='hidden md:block w-7' />
+                    <div className={`absolute w-max ${expand ? "left-1/2 -translate-x-1/2 top-12" : "-top-12 left-0"} opacity-0 group-hover:opacity-100
+                    transition bg-black text-white text-sm px-3 py-2 rounded-lg shadow-lg pointer-events-none`}>
+                        {expand ? 'Close Sidebar' : 'Open Sidebar'}
+                        <div className={`w-3 h-3 absolute bg-black rotate-45 ${expand ? "left-1/2 -top-1.5 -translate-x-1/2" : "left-4 -bottom-1.5"}`}></div>
+                    </div>
+                </div>
+            </div>    
+
+            <button>
+                <Image className={expand ? 'w-6' : 'w-7'} src={expand ? assets.chat_icon : assets.chat_icon_dull} alt=''/>
+                <div>
+                    New Chat!!
+                    <div className='w-3 h-3 absolute bg-black rotate-45 left-4 -bottom-1.5'></div>
+                </div>
+            </button>
+
+        </div>
+    </div>
+  )
+}
+
+export default SideBar
